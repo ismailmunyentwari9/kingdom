304a9fcae64a80abee2c89a3923de614f10d4e4c
ismailmunyentwari9 <ismailmunyentwari9@gmail.com>
2024-02-01 15:30:57 +0200

    Add some fiels and changes in the project

--- src/components/MiddleContact.jsx
+++ src/components/MiddleContact.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const MiddleContact = () => (
-  <div className="MiddleHome w-full lg:h-[80vh] h-[50vh] grid grid-cols-2 ">
+  <div className="MiddleContact w-full lg:h-[80vh] h-[50vh] grid grid-cols-2 ">
     <div className=" col-span-2 lg:col-span-2 lg:w-[70%] ml-10 mt-[20%]">
       <p className="text-[2rem] lg:text-[3rem] text-white font-bold animate-bounce italic">
         <i className="fas fa-quote-left ml-2 text-black" />
